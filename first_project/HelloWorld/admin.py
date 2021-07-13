94e9726af2e575163e2568cb29af7d2b225dee02
stevemeekins <87337285+stevemeekins@users.noreply.github.com>
2021-07-13 12:31:04 -0400

    Add files via upload

---
+++ first_project/HelloWorld/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import AccessRecord,Webpage,Topic
+
+# Register your models here.
+
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
+admin.site.register(Topic)
+# Register your models here.
